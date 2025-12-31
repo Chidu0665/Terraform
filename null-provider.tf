8c98a1ef6b17c87f9ed8b25d9abfc52061860a28
Chidu0665 <chidu0665@gmail.com>
2025-12-31 12:43:25 +0530

    Removed the provider block from the null,random provider file and maintained a centralised required provider file

--- null-provider.tf
+++ null-provider.tf
@@ -1,14 +1,3 @@
-terraform {
-  required_providers {
-    null = {
-      source = "hashicorp/null"
-      version = "3.2.4"
-    }
-  }
-}
-
-
-provider "null" {}
 
 resource "null_resource" "example" {
   provisioner "local-exec" {
--- random-provider.tf
+++ random-provider.tf
@@ -1,13 +1,3 @@
-terraform {
-  required_providers {
-    random = {
-      source = "hashicorp/random"
-      version = "3.7.2"
-    }
-  }
-}
-
-provider "random" {}
 
 resource "random_string" "random" {
   length           = 16
