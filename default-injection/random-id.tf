cc3529d00d09acf466ae94568d67bce98459c053
Chidu0665 <chidu0665@gmail.com>
2026-01-02 21:42:42 +0530

    Random integer demonstrated using interactive way

--- default-injection/random-id.tf
+++ default-injection/random-id.tf
@@ -1,11 +1,11 @@
-variable "byte_length" {
+# variable "byte_length" {
 
-}
+# }
 
-resource "random_id" "example" {
-  byte_length = var.byte_length
-}
+# resource "random_id" "example" {
+#   byte_length = var.byte_length
+# }
 
-output "result"{
-    value=random_id.example.id
-}
+# output "result"{
+#     value=random_id.example.id
+# }
