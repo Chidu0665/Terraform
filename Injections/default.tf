59d7e21f826d260424a8eaac47fe35c2be868783
Chidu0665 <chidu0665@gmail.com>
2026-01-02 13:14:12 +0530

    Demontrated the default way of injecting values for variables

--- Injections/default.tf
+++ Injections/default.tf
@@ -1,12 +1,12 @@
-variable filename {
+variable filename-def {
     default = "default.txt"
     type = string
 }
-variable content {
+variable content-def {
     default = " This is the default content value set"
 }
 resource "local_file" "t2" {
-    filename = var.filename
-    content = var.content
+    filename = var.filename-def
+    content = var.content-def
 }
 
