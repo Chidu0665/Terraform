68d4e277262e12f45d01ba6541e545898592652f
Chidu0665 <chidu0665@gmail.com>
2026-01-03 09:54:44 +0530

    Created a new folder called default-injection and demontrated random-bytes by setting default value for length

---
+++ default-injection/random-byte.tf
@@ -0,0 +1,12 @@
+variable "byte-length" {
+    default=16
+}
+
+resource "random_bytes" "sample" {
+    length=var.byte-length
+}
+
+output "result" {
+    value=random_bytes.sample.base64
+    sensitive=true
+}
