54b2527506aca44aba1034f255ae09f04dc8f216
Chidu0665 <chidu0665@gmail.com>
2026-01-14 10:15:28 +0530

    Authentication of Terraform to Azure(ENV and Provider-Based)

---
+++ App-registration-Azure/main.tf
@@ -0,0 +1,9 @@
+data "azurerm_subscription" "current" {}
+
+output "subscription_id" {
+  value = data.azurerm_subscription.current.subscription_id
+}
+
+output "subscription_name" {
+  value = data.azurerm_subscription.current.display_name
+}
