b0a740a9ecea585d396ab3d9d0bd778a7bb3ffa1
Chidu0665 <chidu0665@gmail.com>
2026-01-16 14:48:29 +0530

    Updated the VM size

--- VM/main.tf
+++ VM/main.tf
@@ -33,7 +33,7 @@ resource "azurerm_linux_virtual_machine" "example" {
   name                = "Chidu-machine"
   resource_group_name = azurerm_resource_group.example.name
   location            = azurerm_resource_group.example.location
-  size                = "Standard_D2a_v4"
+  size                = "Standard_D4a_v4"
   admin_username      = "adminuser"
   network_interface_ids = [
     azurerm_network_interface.example.id,
