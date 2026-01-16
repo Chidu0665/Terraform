aa422a5fdd003e8750ac4fafddb68e221f9b89f7
Chidu0665 <chidu0665@gmail.com>
2026-01-16 15:35:57 +0530

    Updated the Config main.tf

--- VM/main.tf
+++ VM/main.tf
@@ -41,7 +41,7 @@ resource "azurerm_linux_virtual_machine" "example" {
   name                = "Chidu-machine"
   resource_group_name = azurerm_resource_group.example.name
   location            = azurerm_resource_group.example.location
-  size                = "Standard_D4s_v5"
+  size                = "Standard_B2ls_v2"
   admin_username      = "adminuser"
 
   network_interface_ids = [
