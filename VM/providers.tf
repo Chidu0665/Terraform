761d8c5297eee95f37bb6b03b214f1648bc8984f
Chidu0665 <chidu0665@gmail.com>
2026-01-16 13:51:32 +0530

    Config files to create a VM on Azure

--- VM/providers.tf
+++ VM/providers.tf
@@ -20,8 +20,4 @@ terraform {
 }
 provider "azurerm" {
   features {}
-  client_id="6d891c3a-0469-4825-a35d-11a3387af615"
-  client_secret="idP8Q~MRz6h6YP1-6zi5gQ1oc3bEuY1zeHjnzcNX" 
-  tenant_id="9c1e50f3-2148-40bc-b941-0f3e5c9ceeb6"
-  subscription_id="6a6991ac-13fa-4cad-8cb4-87d94302dd32"
 }
