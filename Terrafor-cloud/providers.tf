009fc07101631ecb417f44b59c69290fd6e45eac
Chidu0665 <chidu0665@gmail.com>
2026-01-17 10:58:37 +0530

    Created a demo directory for terraform cloud demo

---
+++ Terrafor-cloud/providers.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    random = {
+      source  = "hashicorp/random"
+      version = "3.7.2"
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = "3.2.4"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = "2.6.1"
+    }
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "~> 3.90"
+    }
+  }
+}
+provider "azurerm" {
+  features {}
+}
